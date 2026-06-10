3a80d063d311b42137aff25af23f99921aa7bc3e
meflebs <meflebs@ac.sce.ac.il>
2026-06-10 14:53:34 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -1,39 +1,25 @@
 import numpy as np
-
-from scipy.optimize import bisect
-
+from scipy import optimize
 
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    """
-    L: אורך במ"מ
-    E: מודול אלסטיות ב-MPa
-    A: שטח חתך בממ"ר
-    r: רדיוס אינרציה במ"מ
-    c: מרחק לסיב קיצוני במ"מ
-    e: אקסצנטריות במ"מ
-    sigma_allow: מאמץ מותר ב-MPa
+  """
+  L: אורך במ"מ
+  E: מודול אלסטיות ב-MPa
+  A: שטח חתך בממ"ר
+  r: רדיוס אינרציה במ"מ
+  c: מרחק לסיב קיצוני במ"מ
+  e: אקסצנטריות במ"מ
+  sigma_allow: מאמץ מותר ב-MPa
 
-    Return: העומס P בניוטון (float)
-    """
+  Return:  P בניוטון (float)
+  """העומס
 
-    def f(P):
-        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
+  def column_stress_error_internal(P):
+    arg = (L / (2 * r)) * np.sqrt(P / (E * A))
+    sec_term = 1 / np.cos(arg)
+    sigma_max = (P / A) * (1 + (e * c / r**2) * sec_term)
+    return sigma_max - sigma_allow
 
-        # sec(theta) = 1/cos(theta)
-        sec_theta = 1.0 / np.cos(theta)
-
-        sigma_max = (P / A) * (
-            1 + (e * c / r**2) * sec_theta
-        )
-
-        return sigma_max - sigma_allow
-
-    # גבולות התחלתיים לחיפוש
-    P_low = 1.0
-    P_high = sigma_allow * A
-
-    # הרחבת התחום במידת הצורך
-    while f(P_low) * f(P_high) > 0:
-        P_high *= 2
-
-    return bisect(f, P_low, P_high)
+  initial_guess_P = 500000
+  P_critical = optimize.newton(column_stress_error_internal, initial_guess_P)
+  return P_critical
