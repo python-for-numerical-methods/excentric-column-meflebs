8412c16cbc1ef8dc27b0ebbd5a60e34ab1659f9b
meflebs <meflebs@ac.sce.ac.il>
2026-06-10 14:19:03 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from scipy.optimize import bisect
 
 
@@ -12,29 +13,27 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
     e: אקסצנטריות במ"מ
     sigma_allow: מאמץ מותר ב-MPa
 
-    Return:
-        P (N)
+    Return: העומס P בניוטון (float)
     """
 
-    def sigma_max(P):
-        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
-@@ -27,14 +14,10 @@ def sigma_max(P):
     def f(P):
-        return sigma_max(P) - sigma_allow
+        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
 
-    # עומס אוילר משמש חסם עליון סביר
-    P_euler = (np.pi**2 * E * A * r**2) / (L**2)
+        # sec(theta) = 1/cos(theta)
+        sec_theta = 1.0 / np.cos(theta)
 
-    # מחפשים תחום שבו יש החלפת סימן
-    P_low = 0.0
-    P_high = 0.99 * P_euler
-    p_min = 1e-8
-    p_max = sigma_allow * A
+        sigma_max = (P / A) * (
+            1 + (e * c / r**2) * sec_theta
+        )
 
-    while f(P_high) < 0:
-        P_high *= 1.5
-    while f(p_max) < 0:
-        p_max *= 2
+        return sigma_max - sigma_allow
 
-    return bisect(f, P_low, P_high, xtol=1e-6)
-    return bisect(f, p_min, p_max)
+    # גבולות התחלתיים לחיפוש
+    P_low = 1.0
+    P_high = sigma_allow * A
+
+    # הרחבת התחום במידת הצורך
+    while f(P_low) * f(P_high) > 0:
+        P_high *= 2
+
+    return bisect(f, P_low, P_high)
