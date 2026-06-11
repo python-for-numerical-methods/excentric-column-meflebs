879066dd7568ed7e5bfe1405f4298a36558b12d1
meflebs <meflebs@ac.sce.ac.il>
2026-06-11 16:53:03 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,15 +1,29 @@
-import numpy as np
-from scipy.optimize import newton
+import math
+from scipy.optimize import bisect
 
 
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
 
-    def f(P):
-        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
-        sigma = (P / A) * (
-            1 + (e * c / r**2) * (1 / np.cos(theta))
-        )
-        return sigma - sigma_allow
+    euler_limit = E * A * ((math.pi * r) / L) ** 2
 
-    P0 = sigma_allow * A
-    return newton(f, P0)
+    def secant_equation(P):
+
+        alpha = (L / (2.0 * r)) * math.sqrt(P / (E * A))
+
+        cos_alpha = math.cos(alpha)
+
+        if abs(cos_alpha) < 1e-12:
+            return float("inf")
+
+        sigma_max = (P / A) * (
+            1.0 + (e * c) / (r**2 * cos_alpha)
+        )
+
+        return sigma_max - sigma_allow
+
+    return bisect(
+        secant_equation,
+        0.0,
+        0.9999 * euler_limit,
+        xtol=1e-6
+    )
