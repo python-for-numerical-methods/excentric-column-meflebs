d3278d4886248eaf59dc8825bc20bd4071c8f45d
meflebs <meflebs@ac.sce.ac.il>
2026-06-11 16:51:45 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,36 +1,15 @@
 import numpy as np
-from scipy.optimize import bisect
+from scipy.optimize import newton
 
 
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    """
-    L: length [mm]
-    E: modulus of elasticity [MPa = N/mm²]
-    A: area [mm²]
-    r: radius of gyration [mm]
-    c: distance to extreme fiber [mm]
-    e: eccentricity [mm]
-    sigma_allow: allowable stress [MPa]
-
-    Returns:
-        Critical load P [N]
-    """
-
-    def sigma_max(P):
-        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
-        sec_theta = 1.0 / np.cos(theta)
-
-        return (P / A) * (
-            1 + (e * c / r**2) * sec_theta
-        )
 
     def f(P):
-        return sigma_max(P) - sigma_allow
+        theta = (L / (2 * r)) * np.sqrt(P / (E * A))
+        sigma = (P / A) * (
+            1 + (e * c / r**2) * (1 / np.cos(theta))
+        )
+        return sigma - sigma_allow
 
-    # Euler load gives a safe upper bound before sec() becomes singular
-    P_euler = (np.pi**2 * E * A * r**2) / (L**2)
-
-    lower = 0.0
-    upper = 0.99 * P_euler
-
-    return bisect(f, lower, upper, xtol=1e-6)
+    P0 = sigma_allow * A
+    return newton(f, P0)
